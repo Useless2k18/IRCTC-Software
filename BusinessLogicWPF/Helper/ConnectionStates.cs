4f1fb98a868a13685917812c8570c7e71a6ae5d2
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-04-23 02:04:36 +0530

    Added Internet Connectivity Checker which will run in background

---
+++ BusinessLogicWPF/Helper/ConnectionStates.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConnectionStates.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the ConnectionStates type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BusinessLogicWPF.Helper
+{
+    using System;
+
+    /// <summary>
+    /// The connection states.
+    /// </summary>
+    [Flags]
+    public enum ConnectionStates
+    {
+        /// <summary>
+        /// Unknown state.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Local system uses a modem to connect to the Internet.
+        /// </summary>
+        Modem = 0x1,
+
+        /// <summary>
+        /// Local system uses a local area network to connect to the Internet.
+        /// </summary>
+        Lan = 0x2,
+
+        /// <summary>
+        /// Local system uses a proxy server to connect to the Internet.
+        /// </summary>
+        Proxy = 0x4,
+
+        /// <summary>
+        /// Local system has RAS (Remote Access Services) installed.
+        /// </summary>
+        RasInstalled = 0x10,
+
+        /// <summary>
+        /// Local system is in offline mode.
+        /// </summary>
+        Offline = 0x20,
+
+        /// <summary>
+        /// Local system has a valid connection to the Internet, but it might or might not be currently connected.
+        /// </summary>
+        Configured = 0x40,
+    }
+}
---
+++ BusinessLogicWPF/Helper/LocalSystemConnection.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LocalSystemConnection.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the LocalSystemConnection type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BusinessLogicWPF.Helper
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The local system connection.
+    /// </summary>
+    public static class LocalSystemConnection
+    {
+        /// <summary>
+        /// The internet get connected state.
+        /// </summary>
+        /// <param name="lpdwFlags">
+        /// The LPDW flags.
+        /// </param>
+        /// <param name="dwReserved">
+        /// The DW reserved.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here.")]
+        [DllImport("wininet.dll", SetLastError = true, CallingConvention = CallingConvention.ThisCall)]
+        public static extern bool InternetGetConnectedState(out ConnectionStates lpdwFlags, long dwReserved);
+
+        /// <summary>
+        /// Retrieves the connected state of the local system.
+        /// </summary>
+        /// <param name="connectionStates">A <see cref="ConnectionStates"/> value that receives the connection description.</param>
+        /// <returns>
+        /// A return value of true indicates that either the modem connection is active,
+        /// or a LAN connection is active and a proxy is properly configured for the LAN.
+        /// A return value of false indicates that neither the modem nor the LAN is connected.
+        /// If false is returned, the <see cref="ConnectionStates.Configured"/> flag may be set
+        /// to indicate that auto-dial is configured to "always dial" but is not currently active.
+        /// If auto-dial is not configured, the function returns false.
+        /// </returns>
+        public static bool IsConnectedToInternet(out ConnectionStates connectionStates)
+        {
+            connectionStates = ConnectionStates.Unknown;
+            return InternetGetConnectedState(out connectionStates, 0);
+        }
+
+        /// <summary>
+        /// Retrieves the connected state of the local system.
+        /// </summary>
+        /// <returns>
+        /// A return value of true indicates that either the modem connection is active,
+        /// or a LAN connection is active and a proxy is properly configured for the LAN.
+        /// A return value of false indicates that neither the modem nor the LAN is connected.
+        /// If false is returned, the <see cref="ConnectionStates.Configured"/> flag may be set
+        /// to indicate that auto-dial is configured to "always dial" but is not currently active.
+        /// If auto-dial is not configured, the function returns false.
+        /// </returns>
+        public static bool IsConnectedToInternet()
+        {
+            return IsConnectedToInternet(out _);
+        }
+    }
+}
