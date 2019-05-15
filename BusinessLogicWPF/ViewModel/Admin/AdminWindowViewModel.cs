ff9e0a0a477433b8da81732989981bb8841023aa
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-05-15 20:27:32 +0530

    Moved DbContext from Admin to Main Window, for Login Support

--- BusinessLogicWPF/ViewModel/Admin/AdminWindowViewModel.cs
+++ BusinessLogicWPF/ViewModel/Admin/AdminWindowViewModel.cs
@@ -10,39 +10,20 @@
 namespace BusinessLogicWPF.ViewModel.Admin
 {
     using System;
-    using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
-    using System.Windows;
 
     using BusinessLogicWPF.DesignAndValidation;
-    using BusinessLogicWPF.GoogleCloudFireStoreLibrary;
-    using BusinessLogicWPF.Helper;
-    using BusinessLogicWPF.Model.Json.Creation;
     using BusinessLogicWPF.Properties;
     using BusinessLogicWPF.View.Admin.UserControls;
 
     using MaterialDesignThemes.Wpf;
-
-    using Newtonsoft.Json;
 
     /// <summary>
     /// The admin window view model.
     /// </summary>
     public class AdminWindowViewModel
     {
-        /// <summary>
-        /// The files.
-        /// </summary>
-        [NotNull]
-        private static readonly string Files = Directory.GetCurrentDirectory();
-
-        /// <summary>
-        /// The secret folder.
-        /// </summary>
-        [NotNull]
-        private static readonly string SecretFolder = Path.Combine(Files, "Secret");
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AdminWindowViewModel"/> class.
         /// </summary>
@@ -59,78 +40,6 @@ namespace BusinessLogicWPF.ViewModel.Admin
             {
                 throw new ArgumentNullException(nameof(snackBarMessageQueue));
             }
-
-            #region Database with Secrets
-            
-            var di = new DirectoryInfo(SecretFolder);
-            var jsonFiles = di.GetFiles("*.json");
-            if (jsonFiles.Length == 0)
-            {
-                MessageBox.Show(
-                    "Please copy the service-account-key.json file into the Secret Folder of the App Directory!\n" +
-                    "Navigate to https://console.developers.google.com/apis/credentials to create a service account key\n" + 
-                    "Note: Please Remember to give Owner permission to the service account key.\n" + 
-                    "Don\'t forget to change your account to uselessgroup2k18@gmail.com first");
-                DataHelper.ExitCode = -1;
-                Application.Current.Shutdown(DataHelper.ExitCode);
-                Process.Start(SecretFolder);
-                Process.Start("https://console.developers.google.com/apis/credentials");
-                return;
-            }
-
-            DataHelper.ExitCode = 0;
-
-            // Google Cloud Platform project ID.
-            const string ProjectId = "ticketchecker-d4f79";
-
-            try
-            {
-                StaticDbContext.ConnectFireStore = new ConnectFireStore(ProjectId, Path.Combine(SecretFolder, jsonFiles[0].FullName));
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.Message);
-                DataHelper.ExitCode = -1;
-                Application.Current.Shutdown(DataHelper.ExitCode);
-            }
-
-            #endregion
-
-            #region Json Folder Path
-
-            if (DataHelper.JsonFolderPath == null)
-            {
-                DataHelper.JsonFolderPath = Path.Combine(Files, "Json");
-            }
-
-            if (DataHelper.ZoneAndDivisionModel == null)
-            {
-                try
-                {
-                    DataHelper.ZoneAndDivisionModel =
-                        JsonConvert.DeserializeObject<ZoneAndDivision>(
-                            File.ReadAllText(Path.Combine(DataHelper.JsonFolderPath, Resources.ZoneAndDivisionJson)));
-                }
-                catch (JsonException e)
-                {
-                    MessageBox.Show(e.Message);
-                }
-            }
-
-            if (DataHelper.StationsList == null)
-            {
-                try
-                {
-                    DataHelper.StationsList = JsonConvert.DeserializeObject<StationsList>(
-                        File.ReadAllText(Path.Combine(DataHelper.JsonFolderPath, Resources.StationsListJson)));
-                }
-                catch (JsonException e)
-                {
-                    MessageBox.Show(e.Message);
-                }
-            }
-
-            #endregion
 
             this.DemoItems = new[]
                                  {
--- BusinessLogicWPF/ViewModel/LoginAndRegister/LoginViewModel.cs
+++ BusinessLogicWPF/ViewModel/LoginAndRegister/LoginViewModel.cs
@@ -9,10 +9,108 @@
 
 namespace BusinessLogicWPF.ViewModel.LoginAndRegister
 {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Windows;
+
+    using BusinessLogicWPF.GoogleCloudFireStoreLibrary;
+    using BusinessLogicWPF.Helper;
+    using BusinessLogicWPF.Model.Json.Creation;
+    using BusinessLogicWPF.Properties;
+
+    using Newtonsoft.Json;
+
     /// <summary>
     /// The login view model.
     /// </summary>
     public class LoginViewModel
     {
+        /// <summary>
+        /// The files.
+        /// </summary>
+        [NotNull]
+        private static readonly string Files = Directory.GetCurrentDirectory();
+
+        /// <summary>
+        /// The secret folder.
+        /// </summary>
+        [NotNull]
+        private static readonly string SecretFolder = Path.Combine(Files, "Secret");
+        
+        public LoginViewModel()
+        {
+            #region Database with Secrets
+            
+            var di = new DirectoryInfo(SecretFolder);
+            var jsonFiles = di.GetFiles("*.json");
+            if (jsonFiles.Length == 0)
+            {
+                MessageBox.Show(
+                    "Please copy the service-account-key.json file into the Secret Folder of the App Directory!\n" +
+                    "Navigate to https://console.developers.google.com/apis/credentials to create a service account key\n" + 
+                    "Note: Please Remember to give Owner permission to the service account key.\n" + 
+                    "Don\'t forget to change your account to uselessgroup2k18@gmail.com first");
+                DataHelper.ExitCode = -1;
+                Application.Current.Shutdown(DataHelper.ExitCode);
+                Process.Start(SecretFolder);
+                Process.Start("https://console.developers.google.com/apis/credentials");
+                return;
+            }
+
+            DataHelper.ExitCode = 0;
+
+            // Google Cloud Platform project ID.
+            const string ProjectId = "ticketchecker-d4f79";
+
+            try
+            {
+                StaticDbContext.ConnectFireStore = new ConnectFireStore(ProjectId, Path.Combine(SecretFolder, jsonFiles[0].FullName));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                DataHelper.ExitCode = -1;
+                Application.Current.Shutdown(DataHelper.ExitCode);
+            }
+
+            #region Json Folder Path
+
+            if (DataHelper.JsonFolderPath == null)
+            {
+                DataHelper.JsonFolderPath = Path.Combine(Files, "Json");
+            }
+
+            if (DataHelper.ZoneAndDivisionModel == null)
+            {
+                try
+                {
+                    DataHelper.ZoneAndDivisionModel =
+                        JsonConvert.DeserializeObject<ZoneAndDivision>(
+                            File.ReadAllText(Path.Combine(DataHelper.JsonFolderPath, Resources.ZoneAndDivisionJson)));
+                }
+                catch (JsonException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+            if (DataHelper.StationsList == null)
+            {
+                try
+                {
+                    DataHelper.StationsList = JsonConvert.DeserializeObject<StationsList>(
+                        File.ReadAllText(Path.Combine(DataHelper.JsonFolderPath, Resources.StationsListJson)));
+                }
+                catch (JsonException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+            #endregion
+
+            #endregion
+        }
     }
 }
